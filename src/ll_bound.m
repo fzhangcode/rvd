60a40f784efd511f11fe85ed77a52b9320217625
Patrick Flaherty <pflahert@stanford.edu>
2012-10-09 02:46:56 -0400

    Accurate ll_bound by plots of M0, u0 and sigma20.

--- src/ll_bound.m
+++ src/ll_bound.m
@@ -2,17 +2,17 @@ function [ ll ] = ll_bound( r, n, u0, sigma20, M0, gam1, gam2, alpha, beta )
 %LL_BOUND Variational bound on data log-likelihood
 [K,J] = size(n);
 
-EqlogPmu = -0.5*log(2*pi*sigma20) -(1/sigma20)*(gam2 + (gam1 - u0).^2);
+EqlogPmu = -0.5*log(2*pi*sigma20) -(0.5/sigma20)*(gam2 + (gam1 - u0).^2);
 EqlogPmu = repmat(EqlogPmu, [K 1]);
 
-for i = 1:length(gam1)
-    EqlogG1(i) = integral(@(x)logG1(x,gam1(i),gam2(i),M0),0,1);
+for j = 1:J
+    EqlogG1(j) = integral(@(x)logG1(x,gam1(j),gam2(j),M0),0,1);
 end
-for i = 1:length(gam1)
-    EqlogG2(i) = integral(@(x)logG2(x,gam1(i),gam2(i),M0),0,1);
+for j = 1:J
+    EqlogG2(j) = integral(@(x)logG2(x,gam1(j),gam2(j),M0),0,1);
 end
 
-EqlogPtheta = gammaln(M0) -repmat(EqlogG1-EqlogG2,[K 1]) ...
+EqlogPtheta = gammaln(M0) -repmat(EqlogG1+EqlogG2,[K 1]) ...
     +(u0*M0-1)*(psi(alpha) - psi(alpha+beta)) ...
     +((1-u0)*M0-1)*(psi(beta) - psi(alpha+beta));
 
@@ -33,8 +33,8 @@ ll = sum(sum(ll));
 end
 
 function [f] = logG1(x, gam1, gam2, M0)
-f = normpdf(x,gam1,sqrt(gam2)).*gammaln(x*M0);
+  f = normpdf(x,gam1,sqrt(gam2)).*gammaln(x*M0);
 end
 function [f] = logG2(x, gam1, gam2, M0)
-f = normpdf(x,gam1,sqrt(gam2)).*gammaln((1-x)*M0);
+  f = normpdf(x,gam1,sqrt(gam2)).*gammaln((1-x)*M0);
 end
