842db385400aed63c4df2eb1c976049715fae622
Patrick Flaherty <pjflaherty@wpi.edu>
2012-10-13 12:32:29 -0400

    Move estimation procedure into function. Change fors to parfors for parallel processing. Note: doing so makes the for loops slower in serial.

--- src/test_ll_bound.m
+++ src/test_ll_bound.m
@@ -18,9 +18,6 @@ end
 n = repmat(100,K,J);
 r = binornd(n,theta);
 
-gam1 = repmat(u0,[1 J]); gam2 = repmat(sigma20, [1 J]);
-alpha = rand([K J]); beta = rand([K J]);
-
 %% Solve for log-likelihood bound
 
 % Compute the log-likelihood bound
@@ -42,58 +39,11 @@ v = logspace(-3 , 2, 50);
 ll_sigma20 = arrayfun(@(x)ll_bound( r, n, u0, x, M0, gam1, gam2, alpha, beta ), v);
 semilogx(v,ll_sigma20) 
 
-for i = 1:10
-%% Open pool
-% matlabpool open
-
-%% Solve for u0
-u0 = mean(gam1);
-
-%% Solve for sigma20
-sigma20 = mean(gam2 + (gam1-u0).^2);
-
-%% Solve for M0
-options = optimset('Display','iter');
-% M0 = fminbnd(@(x)-ll_bound(r, n, u0, sigma20, x, gam1, gam2, alpha, beta),0,1e10)
-M0 = fzero(@(x)dLdM0( x, u0, alpha, beta, gam1, gam2 ), M0, options);
-
-
-%% Solve for alpha & beta
-options = optimset('Display','off');
-for j = 1:J
-    for k = 1:K
-         alpha(k,j) = fzero(@(x)dLdalpha(x, beta(k,j),M0,u0,r(k,j),n(k,j)), alpha(k,j), options);
-    end
-end
-
-options = optimset('Display','off');
-for j = 1:J
-    for k = 1:K
-         beta(k,j) = fzero(@(x)dLdbeta(alpha(k,j), x, M0,u0,r(k,j),n(k,j)), beta(k,j), options);
-    end
-end
-
-%% Solve for gamma1
-
-% gam1 = u0 - (sigma20*M0).*mean(psi(alpha)-psi(beta));
-options = optimset('Display','off');
-for j = 1:J
-    gam1(j)= fzero(@(x)dLdgam1( x, gam2(j), alpha(:,j), beta(:,j), M0, u0, sigma20), gam1(j),...
-        options);
-end
-plot(gam1)
-
-%% Solve for gamma2
-% options = optimset('Display','iter');
-options = optimset('Display','off');
-for j = 1:J
-    gam2(j)= fzero(@(x)dLdgam2( gam1(j), x, M0, sigma20, J ), gam2(j),options);
-end
-plot(gam2)
-disp('Done.')
-
-end
-
+%% Estimate Model Parameters
+matlabpool open
+[ u0,sigma20,M0,alpha,beta,gam1,gam2 ] ...
+    = rvd2_est( r, n, u0, sigma20, M0 );
+matlabpool close
 %%
 
 post_mu = gam1;
