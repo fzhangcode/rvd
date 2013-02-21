6e913680d68a2878ba220d248306f779d1a2050c
Patrick Flaherty <pjflaherty@wpi.edu>
2013-02-21 13:12:02 -0500

    Add generate_sample and complete_ll to rvd2.5.

--- src/python/rvd25/rvd25.py
+++ src/python/rvd25/rvd25.py
@@ -10,36 +10,58 @@ from scipy.special import gammaln, psi, polygamma
 
 import logging
 
+def main():
+    phi = {'mu0':0.1, 'M0':200.0, 'a':1000, 'b':1}
+    r, theta, mu, M = generate_sample(phi, n=100, J=10)
+    print r
+    
+    loglik = complete_ll(phi, r, 100, theta, mu, M)
+    print loglik
+    
 
-def generate_sample(phi, n=1000, N=1, J=1):
-    """Return n samples from the RVD2.5 model with N replicates, 
+def generate_sample(phi, n=100, N=3, J=100):
+    """Returns a from the RVD2.5 model with n reads, N replicates, and
     J locations. The parameters of the model are in the structure phi.
     """
     
     # Draw J location-specific error rates from a Log-Normal
-    mu = np.zeros(J)
+    alpha0 = phi['M0']*phi['mu0']
+    beta0 = phi['M0']*(1-phi['mu0'])
+    mu = ss.beta.rvs(alpha0, beta0, size=J)
     
+    # Draw precision parameter M from Gamma
+    M = ss.gamma.rvs(phi['a'], scale=phi['b'])
+    
+    # Draw sample error rate and error count
+    theta=np.zeros((N,J))
+    r = np.zeros((N,J))
+    for j in xrange(0, J):
+        alpha = mu[j]*M
+        beta = (1-mu[j])*M
+        theta[:,j] = ss.beta.rvs(alpha, beta, size=N)
+        r[:,j] = ss.binom.rvs(n, theta[:,j])
+    return r, theta, mu, M
+
 def complete_ll(phi, r, n, theta, mu, M):
     """ Return the complete data log-likelihood.
     """
+    alpha0 = phi['M0']*phi['mu0']
+    beta0 = phi['M0']*(1-phi['mu0'])
+    
     alpha = mu*M
     beta = (1-mu)*M
     
-    logPmu = lognormal(phi['mu0'], phi['sigma0'])
-    logPM = gamma(phi['a'], phi['b'])
-    logPtheta = beta(alpha, beta)
-    logPr = binomial(r, theta, n)
+    # TODO check for divide by zero in binomial logpdf when theta is close to 0 or 1
+    logPmu = ss.beta.logpdf(mu, alpha0, beta0)
+    logPM = ss.gamma.logpdf(M, phi['a'], phi['b'])
+    logPtheta = ss.beta.logpdf(theta, alpha, beta)
+    logPr = ss.binom.logpmf(r, n, theta)
     
-    return logPmu + logPM + logPtheta + logPr
-    
+    return np.sum(logPmu + logPM + logPtheta + logPr)
 
 def ll(phi, r):
     """ Return the log-likelihood of the data, r, under the model, phi.
     """
-    pass
-    
-
-def main():
     pass
 
 
