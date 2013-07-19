29422335d944779c71fbd0549939a46bb8588c3f
Yuting He <yhe2@wpi.edu>
2013-07-19 01:32:36 -0400

    Created a folder for experiment on MCMC sample size. Adapted codes on generating hdf5 data for different MCMC sample size

---
+++ results/2013-07-18 Test on MCMC Sample Size with Synthetic DNA/Notebook.md
@@ -0,0 +1,41 @@
+2013-07-01 Test Synthetic DNA
+==============================
+
+Purpose
+------------
+To test RVD2.7 on real sequencing data where the truth about the mutation status is know.
+
+Conclusions
+-----------------
+
+Background
+-----------------
+
+Materials and Equipment
+------------------------------
+
+
+Experimental Protocol
+---------------------------
+t test
+
+Results
+-----------
+![dilution=0.1](http://i.imgur.com/KZpsic0.jpg)
+![dilution=0.3](http://i.imgur.com/LJdlpix.jpg)
+![dilution=1](http://i.imgur.com/5xH6g1b.jpg)
+![dilution=10](http://i.imgur.com/0B7Ycm6.jpg)
+![dilution=100](http://i.imgur.com/kPuocDG.jpg)
+
+![tstats](http://i.imgur.com/kHDRhYj.jpg)
+Archived Samples
+-------------------------
+
+Archived Computer Data
+------------------------------
+
+
+Prepared by: _________________________ Date: _____________________
+
+
+Witnessed by: ________________________
---
+++ results/2013-07-18 Test on MCMC Sample Size with Synthetic DNA/runall.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import h5py
+import multiprocessing as mp
+import logging
+
+# <codecell>
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s:%(module)s:%(message)s')
+
+# Insert the src/python/rvd27 directory at front of the path
+rvddir = os.path.join('../../src/python/rvd27')
+sys.path.insert(0, rvddir)
+import rvd27
+
+
+# <codecell>
+
+pool = mp.Pool(processes=60)
+tocfilename = "synthetic_toc.txt"
+toc = pd.read_table(tocfilename)
+
+# <codecell>
+nsample_opt=[4000,40000]
+for i in xrange(2):
+    nsample=nsample_opt[i]
+    logging.debug("SamplingSize="+str(nsample))
+    # Estimate the model for the cases
+    logging.debug("Processing control data.")
+    h5FileName = "SamplingSize="+str(nsample)+"_Control.hdf5"
+    try:
+        with h5py.File(h5FileName, 'r') as f:
+            pass
+    except IOError as e:
+        controlFileList = ["../../data/synthetic_dcs/%s" % filename for filename in toc.Filename[toc.isRef=='Y']]
+        (r, n, loc, refb, ee) = rvd27.load_depth(controlFileList)
+        phi, theta_s, mu_s = rvd27.mh_sample(r, n, nsample=400, burnin=0.2, pool=pool)
+        logging.debug("Saving model in %s" % h5FileName)
+        rvd27.save_model(h5FileName, phi, mu=mu_s, theta=theta_s, r=r, n=n, loc=loc,
+               refb=refb, ee=ee)
+
+    # <codecell>
+
+    # Estimate the model for the cases
+    for dilution in np.unique(toc[toc.isRef=='N'].Dilution):
+        logging.debug("Processing dilution: %0.1f" % dilution)
+        
+        h5FileName = "SamplingSize="+str(nsample)+"_Case%0.1f.hdf5" % dilution
+        h5FileName = h5FileName.replace(".", "_", 1)
+        try:
+            with h5py.File(h5FileName, 'r') as f:
+                pass
+        except IOError as e:
+            caseFileList = ["../../data/synthetic_dcs/%s" % filename for filename in toc.Filename[toc.Dilution==dilution]]
+            (r, n, loc, refb, ee) = rvd27.load_depth(caseFileList)
+            phi, theta_s, mu_s = rvd27.mh_sample(r, n, nsample=500, burnin=0.2, pool=pool)
+            logging.debug("Saving model in %s" % h5FileName)
+            rvd27.save_model(h5FileName, phi, mu=mu_s, theta=theta_s, r=r, n=n,loc=loc,
+               refb=refb, ee=ee)
+
