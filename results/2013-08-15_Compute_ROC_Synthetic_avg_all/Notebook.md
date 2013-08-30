8c08f637f03ee42abe8a4cf95d56bccc930812eb
Yuting He <yhe2@wpi.edu>
2013-08-30 15:07:39 -0400

    Experiment and notebook on Chi-square testing performance. ROC plots are ploted.

--- results/2013-08-15_Compute_ROC_Synthetic_avg_all/Notebook.md
+++ results/2013-08-15_Compute_ROC_Synthetic_avg_all/Notebook.md
@@ -7,26 +7,42 @@ To measure sensitivity and specificity for full RVD2 pipeline for various read d
 
 Conclusions
 -----------------
+The chi-square test lowered the quality of ROC curves. 
 
 Background
 -----------------
-
-
 Materials and Equipment
 ------------------------------
-
-
 Experimental Protocol
 ---------------------------
-
-
 Results
 -----------
-![](ROC_dilution0_1.png)
-![](ROC_dilution0_3.png)
-![](ROC_dilution1_0.png)
-![](ROC_dilution10_0.png)
-![](ROC_dilution100_0.png)
+
+### result with chi-square testing
+In chi-square test, the if the sum of non-reference bases in one position is zero, the chi-squre statistics returned is NaN. For one position if there is a NAN, the testing result will be purely based on Bayesian Hypothesis testing as well. To put it in programming language, I adapted the codes to:
+
+`if postP[i] >0.95 and (chi2P[i] < 0.05/J or np.isnan(chi2P[i]))`
+ 
+ `...`
+ 
+![](ROC_with_chi2.png)
+
+Figure 1. ROC curves from Bayesian Hypothesis Testing with chi-square test 
+
+
+### result without chi-square testing
+
+![](ROC_without_chi2.png)
+Figure 2.ROC curves from Bayesian Hypothesis Testing without chi-square test
+
+
+Up to now I only have generated two dataset, as used in this experiment. 
+Comparing two figures it can be seen
+
+- Chi2 test limited **False Negative Rate**, since even when all positions passed Bayesian Hypothesis Testing, some of them will fail Chi2 test. So that why the ROC curves in Figure 1 can not go up to point (1,1)
+- The Chi2 test didn't improve **True Positive Rate**. On the contrary, in dilution 0.1 and 0.3, the optimal**True Positive Rate** is severely affected by the chi-square test. 
+- To show the curves better, the xstick and ystick of the figures were limited to (-0.01,1.01)
+
 
 Archived Samples
 -------------------------
--- results/2013-08-15_Compute_ROC_Synthetic_avg_all/plotROCbyDilution.py
+++ results/2013-08-15_Compute_ROC_Synthetic_avg_all/plotROCbyDilution.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import h5py
 import logging
 import pdb
+import scipy.stats as ss
 
 # Insert the src/python/rvd27 directory at front of the path
 rvddir = os.path.join('../../src/python/rvd27')
@@ -15,79 +16,101 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)s:%(message)s')
 
 def main():
-    dilutionList = (0.1,0.3,1.0,10.0,100.0)
+    dilutionList = (0.1,0.3,1.0,10.0)
 
-    folderList = ('2013-08-14_Compute_ROC_Synthetic_avg10',\
-              '2013-08-14_Compute_ROC_Synthetic_avg100',\
-              '2013-08-14_Compute_ROC_Synthetic_avg1000', \
-	      '2013-08-19_Compute_ROC_Synthetic_avg10000')
+    folderList = ('2013-08-14_Compute_ROC_Synthetic_avg1000','2013-08-14_Compute_ROC_Synthetic_avg10000',)
 
     
     N=1000 # Z sampling size  
-    (n_gibbs, nmh) = (4000, 50)
-
-    i=0
+    fig=plt.figure(figsize=(10, 9))
+    chi2=False
+    if chi2:
+        ptitle='ROC_with_chi2'
+    else:
+        ptitle='ROC_without_chi2'
+    plt.suptitle(ptitle)
     for d in dilutionList:
         logging.debug("Processing dilution: %0.1f%%" % d)
-        i=i+1
-        plt.figure(i)
+        ax = fig.add_subplot(2,2,dilutionList.index(d)+1)
         label=[]
         for f in folderList:            
             controlFile = "../%s/Control.hdf5" %f
             caseFile = "Case%s.hdf5" % str(d).replace(".","_")
             caseFile = "../%(folder)s/%(file)s" %{'folder':f,'file':caseFile}
-            
              # ROC
-            [fpr,tpr,cov] = ROCpoints(controlFile,caseFile)            
             
-            plt.plot(fpr,tpr, label='%0.1f' % cov)
+            [fpr,tpr,cov] = ROCpoints(controlFile,caseFile,P=0.95,chi2=chi2)            
+            
+            ax.plot(fpr,tpr, label='%0.1f' % cov)
 
-        plt.legend(loc=4)
-        plt.plot([0,1],[0,1],color='k',linestyle='dashed')
-        plt.title('%0.1f%% Mutant Mixture' % d)
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
+        ax.legend(loc=4)
+        ax.plot([0,1],[0,1],color='k',linestyle='dashed')
+        ax.set_title('%0.1f%% Mutant Mixture' % d)
+        ax.set_xlim((-0.01,1.01))
+        ax.set_ylim((-0.01,1.01))
+                 
+        ax.set_xlabel('False Positive Rate')
+        ax.set_ylabel('True Positive Rate')
+        title = 'dilution%0.1f' % d
+        ax.set_title(title.replace('.','_',1))
+    plt.savefig(ptitle)
 
-        title = 'ROC_dilution%0.1f' % d
-        plt.savefig('%s.pdf' % title.replace('.','_',1))
-
-def ROCpoints(controlFile,caseFile,N=1000,P=0.95):
+def ROCpoints(controlFile,caseFile,N=1000,P=0.95,chi2=False):
     # Load the model samples
     (controlPhi, controlTheta, controlMu, controlLoc, controlR, controlN) = rvd27.load_model(controlFile)
     
     (casePhi, caseTheta, caseMu, caseLoc, caseR, caseN) = rvd27.load_model(caseFile)
+
     # Extract the common locations in case and control
     caseLocIdx = [i for i in xrange(len(caseLoc)) if caseLoc[i] in controlLoc]
     controlLocIdx = [i for i in xrange(len(controlLoc)) if controlLoc[i] in caseLoc]
-
+    
     caseMu = caseMu[caseLocIdx,:]
     controlMu = controlMu[controlLocIdx,:]
     caseR = caseR[:,caseLocIdx,:]
     controlR = controlR[:,controlLocIdx,:]
+    caseN = caseN[:,caseLocIdx]
+    controlN = controlN[:,controlLocIdx]
     
     loc = caseLoc[caseLocIdx]
     J = len(loc)
-    #pos = np.arange(85,346,20)
-    idx = (384, 7, 29, 51, 73, 95, 118, 140, 162, 184, 206, 229, 251, 273)
-    #pos = [loc[i] for i in xrange(J) if loc[i] in pos]
+    pos = np.arange(85,346,20)
+    posidx = [i for i in xrange(J) if int(loc[i][8:]) in pos]
 
+    
     # Sample from the posterior Z = muCase - muControl        
-    (Z, caseMuS, controlMuS) = rvd27.sample_post_diff(caseMu-casePhi['mu0'], controlMu-controlPhi['mu0'], N)
+    (Z, caseMuS, controlMuS) = rvd27.sample_post_diff(caseMu, controlMu, N)
     
     # Compute cumulative posterior probability for regions (Threshold,np.inf) 
     pList = [rvd27.bayes_test(Z, [(T, np.inf)]) for T in np.linspace(np.min(np.min(Z)), np.max(np.max(Z)), num=300)]
-
+    
     # mutation classification
     clsList = np.array((np.array(pList)>P).astype(int))
     clsList = clsList.reshape((clsList.shape[0],clsList.shape[1]))# category list
+
+    
+    # chi2 test for goodness-of-fit to a uniform distribution for non-ref bases
+    if chi2:
+        nRep = caseR.shape[0]
+        chi2Prep = np.zeros((J,nRep))
+        chi2P = np.zeros(J)
+        for j in xrange(J):
+                chi2Prep[j,:] = np.array([rvd27.chi2test( caseR[i,j,:] ) for i in xrange(nRep)] )
+                if np.any(np.isnan(chi2Prep[j,:])):
+                    chi2P[j] = 0
+                else:
+                   chi2P[j] = 1-ss.chi2.cdf(-2*np.sum(np.log(chi2Prep[j,:] + np.finfo(float).eps)), 2*nRep) # combine p-values using Fisher's Method
+        
+        clsList2 = np.array((np.array(chi2P)<0.05/J).astype(int))        
+        clsList2 = np.tile(clsList2,(clsList.shape[0],1))
+        clsList = np.array(((clsList+clsList2)==2).astype(int))
     
     # false postive rate
-    fpr = np.array([float(sum(clsList[i])-sum(clsList[i,idx]))/(clsList.shape[1]-len(idx)) \
-           for i in xrange(clsList.shape[0])])
+    fpr = np.array([float(sum(clsList[i])-sum(clsList[i,np.array(posidx)]))/(clsList.shape[1]-len(posidx)) for i in xrange(clsList.shape[0])])
     
     # true positive rate
-    tpr = np.array([float(sum(clsList[i, idx]))/len(idx) for i in xrange(clsList.shape[0])])
-   
+    tpr = np.array([float(sum(clsList[i,np.array(posidx)]))/len(posidx) for i in xrange(clsList.shape[0])])
+
     cov = np.median(caseN)
     return fpr,tpr, cov
 
