744fc8e01c473240efc8db6548e356d3e675cb96
Fan Zhang <fzhang@wpi.edu>
2013-11-25 13:41:47 -0500

    Test rvd29 with different dilutions.

---
+++ results/2013-11-18_test_rvd29_synthetic_data_dc=10/test_rvd29.py
@@ -0,0 +1,57 @@
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import logging
+import pdb
+import scipy.stats as ss
+
+# Insert the source directory at front of the path
+rvddir = os.path.join('./')
+sys.path.insert(0, rvddir)
+import rvd29
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s:%(message)s')
+
+def main():
+    dilutionList = (0.1,0.3,1.0,10.0,100.0)
+
+    folderList = ('./')
+
+    for d in dilutionList:
+        logging.debug("Processing dilution: %0.1f%%" % d)
+        for f in folderList:
+            #path='vcf/%s' % str(10**(folderList.index(f)+1))
+            path='vcf/%s' % str(10*(folderList.index(f)))
+            if not os.path.exists(path):
+                os.makedirs(path)
+            controlFile = "./%s/Control.hdf5" %f
+            caseFile = "Case%s.hdf5" % str(d).replace(".","_")
+            caseFile = "./%(file)s" %{'folder':f,'file':caseFile}
+            
+            outputFile='%(path)s/vcf%(dilution)s' %{'path':path,'dilution':str(d).replace('.','_')}
+           
+            rvd29.test(controlFile,caseFile,T=0.00001,N=1000,outputFile=outputFile)
+if __name__ == '__main__':
+    main()
+
+
+"""import sys
+import os
+import rvd29
+#Insert the source directory at front of the path
+rvddir = os.path.join('./')
+sys.path.insert(0, rvddir)
+
+ControlFile = './Control.hdf5'
+CaseFile = './Case0_1.hdf5'
+OutputFile = 'vcf0_1'
+
+assert os.path.isfile(ControlFile), "ControlFile file does not exist: %s" % ControlFile
+assert os.path.isfile(CaseFile), "CaseFile file does not exist: %s" % CaseFile
+
+rvd29.test(ControlFile, CaseFile, T=0, N=1000, outputFile=OutputFile)
+"""
---
+++ results/2013-11-19_test_rvd29_synthetic_data_dc=10000/runall.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import h5py
+import multiprocessing as mp
+import logging
+import pdb
+# <codecell>
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s:%(module)s:%(message)s')
+
+# Insert the src/python/rvd29 directory at front of the path
+
+rvddir = os.path.join('./rvd29')
+sys.path.insert(0, rvddir)
+import rvd29
+
+##pool =None
+pool = mp.Pool(processes=4)
+tocfilename = "synthetic_toc_p1.txt"
+toc = pd.read_table(tocfilename)
+
+gibbs_nsample = 4000
+mh_nsample = 5
+
+logging.debug("Gibbs step size=%d" % gibbs_nsample)
+logging.debug("mh sample size=%d" % mh_nsample)
+
+# Estimate the model for the control
+logging.debug("Processing control data.")
+h5FileName = "Control.hdf5"
+try:
+    with h5py.File(h5FileName, 'r') as f:
+        pass
+except IOError as e:
+    controlFileList = ["../2013-08-06_Downsample_Read_Depth/depth_chart/10000/%s" % filename for filename in toc.Filename[toc.isRef=='Y']]
+    (r, n, loc, refb) = rvd29.load_depth(controlFileList)
+    phi, theta_s, mu_s, M_s = rvd29.mh_sample(r, n, gibbs_nsample=gibbs_nsample,mh_nsample=mh_nsample, burnin=0.2, pool=pool)
+    logging.debug("Saving model in %s" % h5FileName)
+    rvd29.save_model(h5FileName, phi, mu=mu_s, M=M_s, theta=theta_s, r=r, n=n, loc=loc,
+           refb=refb)
+
+# Estimate the model for the cases
+for dilution in np.unique(toc[toc.isRef=='N'].Dilution):
+    logging.debug("Processing dilution: %0.1f" % dilution)
+    
+    h5FileName = "Case%s.hdf5" % str(dilution).replace(".", "_", 1)
+
+    try:
+        with h5py.File(h5FileName, 'r') as f:
+            pass
+    except IOError as e:
+        caseFileList = ["../2013-08-06_Downsample_Read_Depth/depth_chart/10000/%s" % filename for filename in toc.Filename[toc.Dilution==dilution]]
+        (r, n, loc, refb) = rvd29.load_depth(caseFileList)
+        phi, theta_s, mu_s, M_s = rvd29.mh_sample(r, n, gibbs_nsample=gibbs_nsample,mh_nsample=mh_nsample, burnin=0.2, pool=pool)
+        logging.debug("Saving model in %s" % h5FileName)
+        rvd29.save_model(h5FileName, phi, mu=mu_s, M=M_s, theta=theta_s, r=r, n=n,loc=loc,
+           refb=refb)
+
