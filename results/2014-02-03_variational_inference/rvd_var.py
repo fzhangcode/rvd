03e5d74be7a557d3e83e4302739cca0d5a718e87
Yuting He <yhe2@wpi.edu>
2014-02-04 23:51:04 -0500

    Update variational approach inplementation

--- results/2014-02-03_variational_inference/rvd_var.py
+++ results/2014-02-03_variational_inference/rvd_var.py
@@ -48,12 +48,12 @@ def EqMu(delta, gamma):
 def EqlogMu(delta, gamma):
 	return psi(delta) - psi(delta + gamma)
 
-def Eqlog1_mu(delta, gamma):
+def Eqlog1_Mu(delta, gamma):
 	return psi(gamma) - psi(delta + gamma)
 
 def EqlogGamma(delta, gamma, M):
 	# Eqlog\Gamma(muM)
-	N = 6
+	N = 7
 	mu = range(0, 1 , 2^N+1)
 	y = [kernel(delta, gamma, x, M) for x in mu]
 
@@ -63,18 +63,49 @@ def EqlogGamma(delta, gamma, M):
 def kernel(delta, gamma, mu, M):
 		return ss.beta.pdf(mu, delta, gamma)*betaln(mu*M, (1-mu)*M)
 
+
 ## compute entropy
 def BetaEntropy(a, b):
+	# To compute EqlogQmu and EqlogQtheta
 	#return betaln(delta,gamma) - (delta-1)psi(delta) - (gamma - 1) psi(gamma) +(delta + gamma -2) psi(delta + gamma)
 	return betaln(a,b) - (a-1)psi(a) - (b - 1) psi(b) +(a + b -2) psi(a + b)
 
+
 ## compute ELBO
-def ELBO():
-	EqlogPr = 0
-	EqlogPtheta = 
-	EqlogPmu = 
-	EqlogQtheta = 
-	EqlogQmu = 
+def ELBO(r, n, theta, mu, M, mu0, M0, alpha, beta, delta, gamma):
+	(J,N) = n.shape
+
+	Mu = [EqMu(delta[j], gamma[j]) for j in xrange(J)]
+	logMu = [EqlogMu(delta[j], gamma[j]) for j in xrange(J)]
+	log1_Mu = [Eqlog1_Mu(delta[j], gamma[j]) for j in xrange(J)]
+	logTheta = [EqlogTheta(alpha[j,i], beta[j,i]) for j in xrange(J) for i in xrange(N)]
+	log1_Theta = [Eqlog1_Theta(alpha[j,i], beta[j,i]) for j in xrange(J) for i in xrange(N)]
+
+	EqlogPr = 0.0
+	for j in xrange(J):
+		for i in xrange(N):
+			EqlogPr += - betaln(r[j,i] + 1, n[j,i] - r[j,i] +1)
+			EqlogPr += r[j,i]*logTheta[j,i] + (n[j,i] - r[j,i]) * logTheta[j,i]
+
+	EqlogPtheta = 0.0
+	for j in xrange(J):
+		EqlogPtheta += - betaln(mu[j]*M[j] + np.finfo(float).eps, (1-mu[j]*M[j]) + np.finfo(float).eps)
+		for i in xrange(N):
+			EqlogPtheta += (M[j]* Mu[j]- 1)*logTheta[j,i] +\
+			(M[j]*(1 - Mu[j]) - 1)*log1_Theta[j,i]
+
+	EqlogPmu = -betaln(mu0*M0, (1-mu0)*M0)
+	for j in xrange(J):
+		EqlogPmu += (M0*mu0-1)*logMu[j] + (M0*(1-mu0)-1)*log1_Mu[j]
+
+	EqlogQtheta = 0.0
+	for j in xrange(J):
+		for i in xrange(N):
+			EqlogQtheta += BetaEntropy(alpha[j,i], beta[j,i])
+
+	EqlogQmu = 0.0
+	for j in xrange(J):
+			EqlogQmu += BetaEntropy(delta[j], gamma[j])
 
 
 
