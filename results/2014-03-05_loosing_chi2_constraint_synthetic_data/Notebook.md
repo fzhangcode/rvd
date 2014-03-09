662354ea76edaff6fcd17e390d6dcce73668c371
Yuting He <yhe2@wpi.edu>
2014-03-09 01:06:26 -0500

    Experiment on loosing p-value in chi2 square test (Bonferroni correction) in synthetic dataset

---
+++ results/2014-03-05_loosing_chi2_constraint_synthetic_data/Notebook.md
@@ -0,0 +1,41 @@
+2014-03-04 loosing chi2 p-value constraint on synthetic data
+==============================
+
+Purpose
+------------
+As the chi2 test (with Bonferroni correction) is too strict for HCC1187 test, this experiment aims at testing how loosing chi2 p-value constraint will effect ss/fdr in synthetic dataset.
+
+Conclusions
+-----------------
+Combing the Sensitivity and Specificity table and False Discovery table, the "chi2, mu0=1" option, which means  chi2 p-value is 0.05 without Bonferroni correction and subtracting mu0 in the bayesian hypothesis test achieve the most preferable result.
+
+Background
+----------------
+
+Materials and Equipment
+------------------------------
+
+
+Experimental Protocol
+---------------------------
+
+Results
+-----------
+Please see the histogram for this positions. 
+![]('SS.png')
+From the view of Sensitivity and Specificity, 
+![]('FDR.png')
+
+Combing the Sensitivity and Specificity table and False Discovery table, the "chi2, mu0=1" option, which means  chi2 p-value is 0.05 without Bonferroni correction and subtracting mu0 in the bayesian hypothesis test achieve the most preferable result.
+
+Archived Samples
+-------------------------
+
+Archived Computer Data
+------------------------------
+
+
+Prepared by: Yuting He     Date: 02/24/14
+
+
+Witnessed by: ________________________
---
+++ results/2014-03-05_loosing_chi2_constraint_synthetic_data/OneRep_tau0.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import logging
+import pdb
+import scipy.stats as ss
+
+# Insert the src/python/rvd27 directory at front of the path
+rvddir = os.path.join('../../src/python/rvd27')
+sys.path.insert(0, rvddir)
+import rvd27
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s:%(message)s')
+
+def main():
+    dilutionList = (0.1,0.3,1.0,10.0,100.0)
+
+    folderList = ('one_replicate_synthetic_avg10',\
+                  'one_replicate_synthetic_avg100',\
+                  'one_replicate_synthetic_avg1000',\
+                  'one_replicate_synthetic_avg10000')
+
+    for d in dilutionList:
+        logging.debug("Processing dilution: %0.1f%%" % d)
+    	for f in folderList:
+            path='onerep_vcf_mu0_chi2_05/%s' % str(10**(folderList.index(f)+1))
+            if not os.path.exists(path):
+                os.makedirs(path)
+            controlFile = "../2013-12-20_experiment_set_gibbs_Qsd_mu_1_mu_over_10_minus_mu0/%s/Control.hdf5" %f
+            caseFile = "Case%s.hdf5" % str(d).replace(".","_")
+            caseFile = "../2013-12-20_experiment_set_gibbs_Qsd_mu_1_mu_over_10_minus_mu0/%(folder)s/%(file)s" %{'folder':f,'file':caseFile}
+            
+            outputFile='%(path)s/vcf%(dilution)s' %{'path':path,'dilution':str(d).replace('.','_')}
+           
+            rvd27.test(controlHDF5Name=controlFile,caseHDF5Name=caseFile,diffroi=(0,np.inf),N=1000,outputFile=outputFile)
+if __name__ == '__main__':
+    main()
+
