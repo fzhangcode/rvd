93516269f6909b962545d0a378c969b172c4905f
Yuting He <yhe2@wpi.edu>
2014-03-12 20:21:18 -0400

    Improve the Mu Bar Plot to a better layout

--- results/2014-03-11_combined_somatic_test_HCC1187_dataset/MuBarPlot.py
+++ results/2014-03-11_combined_somatic_test_HCC1187_dataset/MuBarPlot.py
@@ -78,7 +78,7 @@ def MuBarPlot(CallControlMu,CallCaseMu,Loc,title):
     # 95% credible interval
     alpha = 0.05
     pos = int(CallControlMu.shape[1]*alpha/2)
-    #pdb.set_trace()
+    # pdb.set_trace()
     # sort along Gibbs samples
     ControlMu = np.sort(CallControlMu,axis=1)
     Control_yerr = np.array([np.mean(CallControlMu,1)-ControlMu[:,pos], ControlMu[:,CallControlMu.shape[1]-pos]-np.mean(CallControlMu,1)])
@@ -89,32 +89,32 @@ def MuBarPlot(CallControlMu,CallCaseMu,Loc,title):
     if len(Loc) is not 0:
         ind = np.arange(CallCaseMu.shape[0])
         width=0.35
-        fig, ax = plt.subplots()
-##        rects1 = ax.bar(ind, np.mean(CallControlMu,1), width, color='r', yerr=Control_yerr,ecolor='k')
-##        rects2 = ax.bar(ind+width, np.mean(CallCaseMu,1), width, color='g', yerr=Case_yerr,ecolor='k')
+        fig, ax = plt.subplots(figsize=(9,6))
+
         rects1 = ax.bar(ind, np.mean(CallControlMu,1), width, color='0.8', yerr=Control_yerr,ecolor='k')
         rects2 = ax.bar(ind+width, np.mean(CallCaseMu,1), width, color='0.5', yerr=Case_yerr,ecolor='k')
-
+        ax.set_xlim([0, 13])
+    
         ax.set_ylabel('Minor Allele Frequency')
         ax.set_xlabel("HG19 Genomic Location [chr7:154,700,000+X]")
         ax.set_xticks(ind+width)
         
         diffcall = [1,2,3,5,6,8,9,10]
         for i in diffcall:
-            #ax.text(i+width+2-width/9.0, 1.0021,'*',color='b',fontsize=15)
             ax.text(i+width-width/9.0, 1.0021,'*',color='r',fontsize=15)
 
         Loclabel = [x.split(':')[1][4:] for x in Loc]
-        rslabel = ['rs1239326', ' ' ,'rs1239324', '\nrs71534174', ' ' ,'rs35505514', ' ' , ' ' , ' ' , ' ' , ' ' ,'rs4398858', ' ']
+        rslabel = ['rs1239326', ' ' ,'rs1239324', '       rs71534174', ' ' ,'rs35505514', ' ' , ' ' , ' ' , ' ' , ' ' ,'rs4398858', ' ']
  
 
         label = ['%s\n%s' %(Loclabel[i], rslabel[i]) for i in xrange(len(Loclabel))]
 
         ax.set_xticklabels(label)
-##        ax.set_xticklabels( Loc, rotation = 60 )
-        ax.set_ylim
-        lgd = ax.legend( (rects1[0], rects2[0]), ('Control', 'Case'), bbox_to_anchor=(0.8, 1.02, 0.2, .102),
+        
+
+        lgd = ax.legend( (rects1[0], rects2[0]), ('Control', 'Case'), bbox_to_anchor=(0.85, 1.02, 0.15, .102),
                    loc=4,ncol=1, mode="expand", borderaxespad=0., prop={'size':9})
+
         plt.rcParams.update({'font.size': 9, 'font.family': 'serif'})
         fig.tight_layout(rect=[0, 0, 0.95, 0.9])
 
